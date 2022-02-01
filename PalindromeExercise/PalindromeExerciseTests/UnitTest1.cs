c2831e2c3b60ca36cd6ff42e06d7a158c10859cc
antjamespadgett <padgettanthonyj@gmail.com>
2022-02-01 14:38:24 -0600

    Completed 2nd Pali Exercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void MyTest(string word, bool expected)
         {
 
+            //arrange
+            var tester = new WordSmith();
+
+            //act
+            var actaual = tester.isAPalindrome();
+
+            //assert
+            Assert.Equal(expected, actaual);
         }
     }
 }
